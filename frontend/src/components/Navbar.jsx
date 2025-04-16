cca7f9345d2f187f66205e27eea8c51797a3c9c1
sash-84 <s.katale844@gmail.com>
2025-04-16 22:10:46 +0530

    navbar.jsx modified

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,82 +1,190 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { FaBars, FaTimes } from "react-icons/fa";
+import { useAppContext } from "./AppContext";
+import { Link, useNavigate, useLocation } from "react-router-dom";
+import { FaUserCircle } from "react-icons/fa";
 
 const Navbar = () => {
-  const [isOpen, setIsOpen] = useState(false);
 
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const { isLoggedIn, logout } = useAppContext();
+  
+  const [isOpen, setIsOpen] = useState(false);
+  const [showDropdown, setShowDropdown] = useState(false);
+
+  const [username, setUsername] = useState("");
+
+useEffect(() => {
+  const name = localStorage.getItem("user");
+  if (name) {
+    setUsername(name);
+  }
+}, []);
+  
   const scrollToSection = (sectionId) => {
     const section = document.getElementById(sectionId);
     if (section) {
       section.scrollIntoView({ behavior: "smooth" });
     }
-    setIsOpen(false); // Close menu on mobile after clicking
+    setIsOpen(false);
   };
 
-  const handleClick = () => {
-    window.location.href = "http://localhost:5000/"; // Change to your Flask server URL
+  const handleAuth = () => {
+    if (isLoggedIn) {
+      logout();
+      navigate("/");
+    } else {
+      window.open("http://localhost:5000/", "_self");
+    }
+    setIsOpen(false);
   };
+
+  const dropdownRef = useRef(null);
+
+useEffect(() => {
+  const handleClickOutside = (event) => {
+    if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+      setShowDropdown(false);
+    }
+  };
+
+  document.addEventListener("mousedown", handleClickOutside);
+  return () => {
+    document.removeEventListener("mousedown", handleClickOutside);
+  };
+}, []);
+
 
   return (
     <nav className="fixed top-0 left-0 w-full px-6 py-4 bg-gray-900 bg-opacity-80 backdrop-blur-md shadow-md text-white flex justify-between items-center z-50">
-      
-      {/* Logo */}
-      <h1 
-        className="text-2xl text-[#fbb667] font-bold cursor-pointer" 
+      <h1
+        className="text-2xl text-[#fbb667] font-bold cursor-pointer"
         style={{ fontFamily: "Vibur, cursive" }}
-        onClick={() => scrollToSection("hero")}>
-        InstaInsights
+        onClick={() => scrollToSection("hero")}
+      >
+        Insta Insights
       </h1>
 
       {/* Desktop Menu */}
       <ul className="hidden md:flex space-x-8 text-md items-center">
-        <li className="nav-links" onClick={() => scrollToSection("hero")}>
-          Home
-        </li>
-        <li className="nav-links" onClick={() => scrollToSection("features")}>
-          Features
-        </li>
-        <li className="nav-links" onClick={() => scrollToSection("how-it-works")}>
-          How It Works
-        </li>
-        <li>
-        <button
-            onClick={handleClick}
-            className="px-4 py-1 rounded-full font-semibold hover:scale-105 transition-all transform duration-300">
+        {location.pathname != "/" && (
+          <li className="nav-links">
+            <Link to={"/"}>Home</Link>
+          </li>
+        )}
+        {location.pathname === "/" && (
+          <>
+            <li className="nav-links" onClick={() => scrollToSection("hero")}>
+              Home
+            </li>
+            <li className="nav-links" onClick={() => scrollToSection("features")}>
+              Features
+            </li>
+            <li className="nav-links" onClick={() => scrollToSection("how-it-works")}>
+              How It Works
+            </li>
+            <li className="nav-links" onClick={() => scrollToSection("contact")}>
+              Contact Us
+            </li>
+          </>
+        )}
+        {!isLoggedIn ? (
+          <>
+            <li>
+              <button
+                onClick={handleAuth}
+                className="bg-gradient-to-r from-[#fcce9f] to-[#fbb667] text-gray-900 px-4 py-1 rounded-full font-semibold hover:scale-105 transition-all transform duration-300"
+              >
                 Login
-        </button>
-        </li>
+              </button>
+            </li>
+          </>
+        ) : (
+          <>
+          <li ref={dropdownRef} className="relative">
+            <button className='flex items-center space-x-2' onClick={() => {setShowDropdown(!showDropdown)}}>
+              <FaUserCircle className='text-[#fbb667] text-3xl'/>
+              <span className='text-[#fbb667] hidden sm:inline font-semibold'>{username}</span>
+            </button>
+          
+
+          {showDropdown && (
+              <div className='absolute right-0 mt-2 w-48 bg-gray-800 shadow-md rounded-lg p-2 flex flex-col justify-center items-center space-y-4'>
+                <p className='font-semibold text-center'>Welcome, {username}</p>
+                {location.pathname === "/" && (
+                  <Link to={"/dashboard"} className="nav-links" onClick={() => setShowDropdown(false)}>Dashboard</Link>
+                )}
+                <button 
+                  onClick={handleAuth} 
+                  className="bg-gradient-to-r from-[#fcce9f] to-[#fbb667] text-gray-900 px-4 py-1 rounded-full font-semibold hover:scale-105 transition-all transform duration-300">Logout</button>
+              </div>
+            )}
+            </li>
+          </>
+        )}
       </ul>
 
       {/* Mobile Menu Button */}
-      <button className="md:hidden text-[#fbb667] focus:outline-none bg-transparent" onClick={() => setIsOpen(!isOpen)}>
-        {isOpen ? <FaTimes /> : <FaBars />}
-      </button>
+      <div className="md:hidden z-50 text-[#fbb667]" onClick={() => setIsOpen(!isOpen)}>
+        {isOpen ? <FaTimes size={24} /> : <FaBars size={24} />}
+      </div>
 
-      {/* Mobile Dropdown Menu */}
+      {/* Mobile Menu Dropdown */}
       {isOpen && (
-        <div className="absolute top-16 right-6 w-48 bg-gray-800 rounded-lg shadow-lg flex flex-col items-center md:hidden">
-          <ul className="w-full text-center py-2">
-            <li className="py-2 border-b border-gray-700" onClick={() => scrollToSection("hero")}>
-              Home
+        <ul className="absolute top-20 left-0 w-full bg-gray-900 text-white flex flex-col items-center space-y-6 py-6 shadow-md md:hidden z-40">
+          {location.pathname === "/dashboard" && (
+            <li className="nav-links">
+              <Link to={"/"} onClick={() => setIsOpen(false)}>Home</Link>
             </li>
-            <li className="py-2 border-b border-gray-700" onClick={() => scrollToSection("features")}>
-              Features
-            </li>
-            <li className="py-2 border-b border-gray-700" onClick={() => scrollToSection("how-it-works")}>
-              How It Works
-            </li>
-            <li className="py-2">
-              <button
-                onClick={handleClick}
-                className="px-4 py-1 font-semibold rounded-full">
+          )}
+          {location.pathname === "/" && (
+            <>
+              <li className="nav-links" onClick={() => scrollToSection("hero")}>
+                Home
+              </li>
+              <li className="nav-links" onClick={() => scrollToSection("features")}>
+                Features
+              </li>
+              <li className="nav-links" onClick={() => scrollToSection("how-it-works")}>
+                How It Works
+              </li>
+              <li className="nav-links" onClick={() => scrollToSection("contact")}>
+                Contact Us
+              </li>
+            </>
+          )}
+          {!isLoggedIn ? (
+            <>
+              <li>
+                <button
+                  onClick={handleAuth}
+                  className="bg-gradient-to-r from-[#fcce9f] to-[#fbb667] text-gray-900 px-4 py-1 rounded-full font-semibold hover:scale-105 transition-all transform duration-300"
+                >
                   Login
-              </button>
-            </li>
-          </ul>
-        </div>
+                </button>
+              </li>
+            </>
+          ) : (
+            <>
+              <li className="nav-links">
+                <Link to={"/dashboard"} onClick={() =>setIsOpen(false)}>Dashboard</Link>
+              </li>
+              <li>
+                <button
+                  onClick={handleAuth}
+                  className="bg-gradient-to-r from-[#fcce9f] to-[#fbb667] text-gray-900 px-4 py-1 rounded-full font-semibold hover:scale-105 transition-all transform duration-300"
+                >
+                  Logout
+                </button>
+              </li>
+            </>
+          )}
+        </ul>
       )}
     </nav>
   );
 };
 
-export default Navbar; 
+export default Navbar;
